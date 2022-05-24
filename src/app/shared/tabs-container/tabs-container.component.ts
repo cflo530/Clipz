fe5b81df74a64055971485ea0c8e623d2cacb326
Carlos Flores <cflo530@me.com>
2022-05-23 17:34:33 -0700

    inital commit

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,19 @@
+import { Component, AfterContentInit, ContentChild, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChild(TabComponent) tabs?: QueryList<TabComponent>
+
+  constructor() { }
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
+  }
+
+}
