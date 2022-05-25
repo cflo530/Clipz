cde447c3555fec7642ff07a25faa579b32c29e91
Carlos Flores <cflo530@me.com>
2022-05-24 17:33:04 -0700

    finished component design and fixed modal issue

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterContentInit, ContentChild, QueryList } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
 import { TabComponent } from '../tab/tab.component';
 
 @Component({
@@ -8,12 +8,26 @@ import { TabComponent } from '../tab/tab.component';
 })
 export class TabsContainerComponent implements AfterContentInit {
 
-  @ContentChild(TabComponent) tabs?: QueryList<TabComponent>
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>
 
   constructor() { }
 
   ngAfterContentInit(): void {
-    console.log(this.tabs);
+    const activeTabs = this.tabs?.filter(tab => tab.active);
+
+    if(!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
+  }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+
+    tab.active = true;
+
+    return false;
   }
 
 }
